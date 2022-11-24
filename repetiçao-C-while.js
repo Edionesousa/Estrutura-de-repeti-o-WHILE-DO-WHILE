874929e28137a7cf3142992bbba8b2394c8ebe84
Edione sousa <edione.compras@gmail.com>
2022-11-24 18:02:09 -0300

    first commit

---
+++ repetiçao-C-while.js
@@ -0,0 +1,9 @@
+
+
+let c = 1
+while(c <= 8|| c <= 1){
+    console.log(`Comer ${c}`)
+    c++
+ 
+} 
+console.log('fim da pizaa')
---
+++ repetiçao-F-do-while.js
@@ -0,0 +1,6 @@
+var c = 1
+do{
+    console.log(`Comer ${c}`)
+    c++
+}while(c <=8|| c <= 8)
+console.log('Fim da pizza, vai pedir outra?')
